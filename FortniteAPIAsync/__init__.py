fce381ad37243f40d3c0a23019c56313d51c33dd
Oli <55298769+xMistt@users.noreply.github.com>
2026-02-07 05:10:11 +0000

    add missing imports to init

--- FortniteAPIAsync/__init__.py
+++ FortniteAPIAsync/__init__.py
@@ -5,3 +5,12 @@ __version__ = '2.0.0'
 from .api import APIClient
 from .exceptions import *
 from .enums import *
+from .aes import *
+from .banners import *
+from .creator_code import *
+from .map import *
+from .news import *
+from .playlists import *
+from .shop import *
+from .stats import *
+from .cosmetics import *
