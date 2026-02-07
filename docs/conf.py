7b439031c3c6866757664f5dd077a5950bc6ebf5
Oli <55298769+xMistt@users.noreply.github.com>
2026-02-07 05:13:38 +0000

    remove template files

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,6 @@ rst_prolog = """
 """
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
